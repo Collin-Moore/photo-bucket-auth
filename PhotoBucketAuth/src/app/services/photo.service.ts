d30200a284faafe8a29ced56814903da626d677f
Collin Moore <collin.moore96@gmail.com>
2017-07-22 16:39:51 -0500

    just so there is proof of it, this would work if I could make a click event for tabs, instead I will just make two streams

--- PhotoBucketAuth/src/app/services/photo.service.ts
+++ PhotoBucketAuth/src/app/services/photo.service.ts
@@ -1,20 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Query } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
 import { AuthService } from "./auth.service";
 import { Observable } from "rxjs/Observable";
 import { Photo } from "../models/photo";
+import { Subject } from "rxjs/Subject";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import 'rxjs/add/operator/switchMap';
 
 
 @Injectable()
 export class PhotoService {
 
   readonly photosPath = "photos";
-  public photoStream: FirebaseListObservable<Photo[]>;
+  // public photoStream: FirebaseListObservable<Photo[]>;
+  public photoStream: Observable<Photo[]>;
+  public isMyPhotoTabStream: Subject<boolean>;
 
   constructor(private db: AngularFireDatabase, private authService: AuthService) {
-    if(authService.isSignedInStream) {
-      this.photoStream = this.db.list(this.photosPath);
-    }
+    // if(authService.isSignedInStream) {
+    //   this.photoStream = this.db.list(this.photosPath);
+    // }
+    this.isMyPhotoTabStream = new BehaviorSubject<boolean>(true);
+
+    this.photoStream = this.isMyPhotoTabStream.switchMap<boolean, Photo[]>(
+      (isMyPhotos: boolean) => {
+        if (isMyPhotos) {
+          return this.db.list(this.photosPath, {
+            query: {
+              orderByChild: 'uid',
+              equalTo: this.authService.currentUserUid
+            }
+          });
+        } else {
+          return this.db.list(this.photosPath);
+        }
+      });
   }
 
+  showOnlyMyPhotos(isMyPhotosTab: boolean): void {
+    console.log("showOnlyMyPhotosTab");
+    this.isMyPhotoTabStream.next(isMyPhotosTab);
+  }
 }
