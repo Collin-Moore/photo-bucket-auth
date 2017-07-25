0325921ca0a95fe34224707f702f8536e7bad70c
Collin Moore <collin.moore96@gmail.com>
2017-07-24 21:53:28 -0500

    edit works

--- PhotoBucketAuth/src/app/photo-display-card/photo-display-card.component.ts
+++ PhotoBucketAuth/src/app/photo-display-card/photo-display-card.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Photo } from "../models/photo";
 import { Router } from "@angular/router";
 import { PhotoService } from "../services/photo.service";
+import { MdDialogConfig, MdDialog } from "@angular/material";
+import { PhotoDialogComponent } from "../photo-dialog/photo-dialog.component";
 
 @Component({
   selector: 'app-photo-display-card',
@@ -10,12 +12,21 @@ import { PhotoService } from "../services/photo.service";
 })
 export class PhotoDisplayCardComponent implements OnInit {
   @Input() photo: Photo;
-  constructor(private router: Router, private photoService: PhotoService) { }
+  @Input() isMyPhoto: boolean;
+  constructor(private router: Router, private photoService: PhotoService, private dialog: MdDialog) { }
 
   ngOnInit() {
   }
 
   goToDetailedPage() {
     this.router.navigate(['/photo', this.photo.$key]);
+  }
+
+  edit(): void {
+    const dialogConfig = new MdDialogConfig();
+    dialogConfig.data = {
+      photo: this.photo
+    };
+    this.dialog.open(PhotoDialogComponent, dialogConfig);
   }
 }
